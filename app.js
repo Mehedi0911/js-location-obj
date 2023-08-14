a5ec64f0950090d200b97facb0077a8e5f313540
mehedi.mosh <mehedi.mosh@gmail.com>
2023-08-14 10:29:24 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+const urlContainer = document.getElementById("my-url")
+const hostNameContainer = document.getElementById("my-hostname")
+const postContainer = document.getElementById("my-port")
+
+
