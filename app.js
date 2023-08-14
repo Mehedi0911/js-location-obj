1e69094bb169704dc38271a799bd6f9e6c45241d
mehedi.mosh <mehedi.mosh@gmail.com>
2023-08-14 12:33:49 +0600

    finished the project

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const hostNameSpan = document.getElementById("site-hostname")
 const postSpan = document.getElementById("site-port")
 const protocolSpan = document.getElementById("site-protocol")
 const pathSpan = document.getElementById("site-path")
+console.log(location.origin)
+urlSpan.innerText = location.href;
+originSpan.innerText = location.origin;
+hostNameSpan.innerText = location.hostname;
+postSpan.innerText = location.port;
+protocolSpan.innerText = location.protocol;
+pathSpan.innerText = location.pathname;
 
+function locationReload() {
+    location.reload()
+}
+function replaceLocation() {
+    location.replace('https://www.w3schools.com/')
+}
+function assignLocation() {
+    location.assign('http://127.0.0.1:5500/blog.html')
+}
 
 
